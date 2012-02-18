2c4f482764ccb31a82e8233249aee610d7921f69
tanaki <nipigelet@gmail.com>
2012-02-18 10:45:31 +0000

    add analytics

--- mobile/index.php
+++ mobile/index.php
@@ -81,5 +81,19 @@
 			Credits : <a href="mailto:nico@nicolaspigelet.com">Nicolas Pigelet</a> - Design by : <a href="http://frederikdelmotte.be">Frederik Delmotte</a>
 		</div>
 		
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-3568983-3']);
+		  _gaq.push(['_setDomainName', 'nicolaspigelet.com']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
 	</body>
 </html>
